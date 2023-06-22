9e3b1d621b0c5eb0b2a4a64509af1133d801532d
Preeti Dantkale <dantkalepreeti29@gmail.com>
2023-06-22 16:35:49 +0530

    Project create API

---
+++ src/main/java/com/josh/intranet/controller/ProjectController.java
@@ -0,0 +1,41 @@
+package com.josh.intranet.controller;
+
+import com.josh.intranet.dto.request.ProjectRequestDto;
+import com.josh.intranet.service.ProjectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class ProjectController {
+
+  @Autowired
+  ProjectServiceImpl projectService;
+
+  @PostMapping("/api/v1/projects")
+  public ResponseEntity<Map> createProject(@Validated @RequestBody ProjectRequestDto projectRequestDto
+      , BindingResult bindingResult){
+    if (bindingResult.hasErrors()) {
+      List<String> errorMessages = bindingResult.getFieldErrors().stream()
+          .map(FieldError::getDefaultMessage)
+          .collect(Collectors.toList());
+      Map<String, String> response = new HashMap<>();
+      response.put("message", errorMessages.toString());
+      return ResponseEntity.badRequest().body(response);
+    }
+    projectService.createProject(projectRequestDto);
+    Map<String, String> response = new HashMap<>();
+    response.put("message", "Project Added Successfully");
+    return ResponseEntity.ok().body(response);
+  }
+}
---
+++ src/main/java/com/josh/intranet/dto/request/ProjectRequestDto.java
@@ -0,0 +1,19 @@
+package com.josh.intranet.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectRequestDto {
+  private Long id;
+  @NotNull(message = "active is required")
+  private boolean active;
+  @NotNull(message = "Name is required")
+  private String name;
+}
